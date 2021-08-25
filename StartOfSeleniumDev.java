12d3b8252f9bff422dd926b248183b87bf4e3750
LEV1970 <80787604+LEV1970@users.noreply.github.com>
2021-08-25 16:21:29 +0300

    Delete StartOfSeleniumDev.java

--- StartOfSeleniumDev.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.lesson3;
-
-public class StartOfSeleniumDev {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", src/main/resources/chromedriver.exe);
-    }
-}
