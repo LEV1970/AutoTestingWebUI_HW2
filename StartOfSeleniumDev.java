2245582456e4a0a195c05cda6c69dcbf79557709
LEV1970 <80787604+LEV1970@users.noreply.github.com>
2021-08-25 16:16:23 +0300

    Add files via upload

---
+++ StartOfSeleniumDev.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.lesson3;
+
+public class StartOfSeleniumDev {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", src/main/resources/chromedriver.exe);
+    }
+}
